fa73b37c6da0e95bd5596b74cc73a0b5f2e9351c
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-06-18 20:14:55 -0300

    FEAT: Implementing Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-100 text-center mt-10 p-10'>
+      <h2 className='text-[14px] '> Precisa de Ajuda? Email: contact@blogdaweb.com </h2>
+      <h3 className='text-[14px] mt-3'> Copyright © 2024 Luis Braga Dev</h3>
+    </div>
   )
 }
