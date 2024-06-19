6bf7668bdc3c974b83780d87ade0464ca94d26c4
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-06-19 15:26:18 -0300

    FIX: Add navigate to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
       <img src={logo} className='w-[180px] cursor-pointer' onClick={() => navigate('/')}/>
       
       <ul className='flex gap-4 md:gap-14 flex-wrap'>
-        <li className='hover:font-bold cursor-pointer'> Home </li>
+        <li className='hover:font-bold cursor-pointer' onClick={() => navigate('/')}> Home </li>
         <li className='hover:font-bold cursor-pointer'> About Us </li>
         <li className='hover:font-bold cursor-pointer'> Contact Us </li>
       </ul>
